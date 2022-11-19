460e4af475537044b7f11c4b1d415d0f1b148e8d
Talal <talalahmad257@gmail.com>
2022-11-19 10:03:58 +0000

    ESLint

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,73 @@
-import fetch from "node-fetch";
-import { load } from "cheerio";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import fetch from 'node-fetch'
+import { load } from 'cheerio'
 
 const urls = {
-  blog: "https://blog.counter-strike.net/",
-  updates: "https://blog.counter-strike.net/index.php/category/updates/"
-};
+  blog: 'https://blog.counter-strike.net/',
+  updates: 'https://blog.counter-strike.net/index.php/category/updates/'
+}
 
 /**
  * Uses this url: "https://blog.counter-strike.net/"
  * @param index - The page index, first page (default) is 0
  * @returns {Promise<Post[]>}
  */
-export async function getBlogPosts(index: number = 0) {
-  let url = urls.updates;
-  index && (url += `page/${index}/`);
-  return getPosts(urls.blog);
+export async function getBlogPosts (index: number = 0): Promise<Post[]> {
+  let url = urls.updates
+  ;(Boolean(index)) && (url += `page/${index}/`)
+  return await getPosts(urls.blog)
 }
-
 
 /**
  * Uses this url: https://blog.counter-strike.net/index.php/category/updates/
  * @param index - The page index, first page (default) is 0
  * @returns {Promise<Post[]>}
  */
-export async function getUpdatePosts(index: number = 0) {
-  let url = urls.updates;
-  index && (url += `page/${index}/`);
-  return getPosts(url);
+export async function getUpdatePosts (index: number = 0): Promise<Post[]> {
+  let url = urls.updates
+  ;(Boolean(index)) && (url += `page/${index}/`)
+  return await getPosts(url)
 }
 
 /**
- * 
+ *
  * @param url - The url to scrape
  * @returns {Promise<Post[]>}
  */
-export default async function getPosts(url: string): Promise<Post[]> {
-  const res = await fetch(url),
-    data = await res.arrayBuffer(),
-    buffer = Buffer.from(data),
-    $ = load(buffer),
-    post_container = $.root().find("#post_container"),
-    posts = post_container
-      .children()
-      .filter(".inner_post")
-      .map((_, el) => {
-        const post = $(el),
-          title = post.find("h2").find("a").text(),
-          link = post.find("h2").find("a").attr("href")!,
-          date = new Date(post.find("p").filter(".post_date").text().split(" ")[0]),
-          image =
-            post.find("img").length > 1
-              ? post.find("img").eq(1).attr("src")!
-              : post.find("img").attr("src")!,
-          content = post
-            .find("p")
-            .slice(1)
-            .toArray()
-            .map(el => $(el).text())
-            .join("\n\n");
-        return { title, link, date, image, content };
-      })
-      .toArray();
-  return posts;
+export default async function getPosts (url: string): Promise<Post[]> {
+  const res = await fetch(url)
+  const data = await res.arrayBuffer()
+  const buffer = Buffer.from(data)
+  const $ = load(buffer)
+  const postContainer = $.root().find('#post_container')
+  const posts = postContainer
+    .children()
+    .filter('.inner_post')
+    .map((_, el) => {
+      const post = $(el)
+      const title = post.find('h2').find('a').text()
+      const link = post.find('h2').find('a').attr('href')!
+      const date = new Date(post.find('p').filter('.post_date').text().split(' ')[0])
+      const image =
+            post.find('img').length > 1
+              ? post.find('img').eq(1).attr('src')!
+              : post.find('img').attr('src')!
+      const content = post
+        .find('p')
+        .slice(1)
+        .toArray()
+        .map(el => $(el).text())
+        .join('\n\n')
+      return { title, link, date, image, content }
+    })
+    .toArray()
+  return posts
 }
 
 export interface Post {
-  title: string;
-  link: string;
-  date: Date;
-  image: string;
-  content: string;
+  title: string
+  link: string
+  date: Date
+  image: string
+  content: string
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,15 @@
-import { getBlogPosts, getUpdatePosts } from "../src/index";
+import { getBlogPosts, getUpdatePosts } from '../src/index'
 
-describe("getBlogPosts", () => {
-  it("should return an array of posts", async () => {
-    const posts = await getBlogPosts();
-    expect(posts).toBeInstanceOf(Array);
-  });
-});
+describe('getBlogPosts', () => {
+  it('should return an array of posts', async () => {
+    const posts = await getBlogPosts()
+    expect(posts).toBeInstanceOf(Array)
+  })
+})
 
-describe("getUpdatePosts", () => {
-  it("should return an array of posts", async () => {
-    const posts = await getUpdatePosts();
-    expect(posts).toBeInstanceOf(Array);
-  });
-});
+describe('getUpdatePosts', () => {
+  it('should return an array of posts', async () => {
+    const posts = await getUpdatePosts()
+    expect(posts).toBeInstanceOf(Array)
+  })
+})
