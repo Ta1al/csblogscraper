6b09ad0cc3cc9e07c294341bee6c62e8d553bc5f
Talal <talalahmad257@gmail.com>
2022-11-25 06:17:04 +0000

    Add more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,11 +5,29 @@ describe('getBlogPosts', () => {
     const posts = await getBlogPosts()
     expect(posts).toBeInstanceOf(Array)
   })
+
+  it('should return an array of posts', async () => {
+    const posts = await getBlogPosts(4)
+    expect(posts).toBeInstanceOf(Array)
+  })
+
+  it('should throw an error', async () => {
+    await expect(getBlogPosts(1000)).rejects.toThrow()
+  })
 })
 
 describe('getUpdatePosts', () => {
   it('should return an array of posts', async () => {
     const posts = await getUpdatePosts()
     expect(posts).toBeInstanceOf(Array)
+  })
+
+  it('should return an array of posts', async () => {
+    const posts = await getUpdatePosts(4)
+    expect(posts).toBeInstanceOf(Array)
+  })
+
+  it('should throw an error', async () => {
+    await expect(getUpdatePosts(1000)).rejects.toThrow()
   })
 })
